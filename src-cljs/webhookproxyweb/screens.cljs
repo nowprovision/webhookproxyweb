3bf7ebfe620f0305c4e8ab193b98fcea97478e1b
Matt Freeman <matt@nowprovision.com>
2015-11-01 11:55:38 +0700

    refactor and small optimize based on first vec

--- src-cljs/webhookproxyweb/screens.cljs
+++ src-cljs/webhookproxyweb/screens.cljs
@@ -1,13 +1,21 @@
 (ns webhookproxyweb.screens
   (:require-macros [reagent.ratom :refer [reaction]])
-  (:require [re-frame.core :refer [register-sub register-handler]]))
+  (:require [re-frame.db :refer [app-db]]
+            [re-frame.core :refer [register-sub register-handler]]))
 
-(register-sub :screen-changed (fn [db [_ & filters]]
-                                  (reaction 
-                                    (let [filter-len (count filters)
-                                          active-screen (:active-screen @db)]
-                                      (when (= (seq (take filter-len active-screen)) filters)
-                                        (drop filter-len active-screen))))))
+(def active-screen (reaction (-> app-db deref :active-screen)))
+
+(defn screen-changed 
+  "when filters is [:webhooks] - e.g. (subscribe [:screen-changed :webhooks])
+  and active-screen is a vector of full path [:webhooks :bar :foo]
+  return a new reaction producing only [:bar :foo]"
+  [db [_ & filters]]
+  (reaction 
+    (let [filter-len (count filters)]
+      (when (= (seq (take filter-len @active-screen)) filters)
+        (drop filter-len @active-screen)))))
+
+(register-sub :screen-changed screen-changed)
 
 (defn change-screen [db path ]
   (let [path-components (rest path)]
